d7c2cee863851d05548f7ad9c4096ffa0deb331b
TheAlexGo <alex280702@mail.ru>
2021-09-27 11:59:27 +0300

    Выполнение третьего варианта

--- Praktikum_2/MainWindow.xaml.cs
+++ Praktikum_2/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Praktikum_2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxArea.Text = "";
+        }
     }
 }
