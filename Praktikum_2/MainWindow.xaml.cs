5768975476fe0a31057f51d23008777abb13be02
TheAlexGo <alex280702@mail.ru>
2021-09-27 10:55:40 +0300

    Выполнение девятого задания

--- Praktikum_2/MainWindow.xaml.cs
+++ Praktikum_2/MainWindow.xaml.cs
@@ -7,7 +7,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -24,27 +23,6 @@ namespace Praktikum_2
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void inkCanvas_Gesture(object sender, InkCanvasGestureEventArgs e)
-        {
-            String gestures = "";
-            // Выборка "предпологаемых" гестур.
-            foreach (GestureRecognitionResult res in e.GetGestureRecognitionResults())
-                gestures += res.ApplicationGesture.ToString() + "; ";
-            // Отображаем название гестуры.
-            gestureName.Text = gestures;
-        }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            // Выборка всех режимов редактирования InkCanvas.
-            foreach (InkCanvasEditingMode mode in
-           Enum.GetValues(typeof(InkCanvasEditingMode)))
-                lstEditingMode.Items.Add(mode);
-            lstEditingMode.SelectedItem = inkCanvas.EditingMode;
-        }
-        private void lstEditingMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
         }
     }
 }
